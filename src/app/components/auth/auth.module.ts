361a3c74c7f80944763eab6af4125a89201ae9d1
Wagner Freitas <wagner.freitas@getconnect.com.br>
2024-09-04 08:42:13 -0300

    feat: austes normais

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // Import RouterModule from the correct location
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
