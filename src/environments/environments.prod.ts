12bba247d28f5cae6f338711905f4580d9800c4a
Wagner Freitas <wagner.freitas@getconnect.com.br>
2024-09-23 07:47:33 -0300

    feat: alterações no package

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDeXRHv6Ds2Wd3kLgzDKsxnZEGI2HcQ51w",
+    authDomain: "matget-72c4f.firebaseapp.com",
+    projectId: "matget-72c4f",
+    storageBucket: "matget-72c4f.appspot.com",
+    messagingSenderId: "155911193858",
+    appId: "1:155911193858:web:92d92de8f27b885e782e1b",
+    measurementId: "G-1TVTBGRTXV"
+  }
+}
